985ef54a87d2869fb8286253cdbcea6d1d1fd8c5
SURYANSH SHRIVASTAVA <39276431+suryanshsh@users.noreply.github.com>
2019-03-20 21:22:41 +0530

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -1,5 +1,6 @@
 
 library(plyr)
+#importing data training, testing and subject
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
@@ -11,23 +12,26 @@ y_test<- read.table("./UCI HAR Dataset/test/y_test.txt")
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-
+#importing features
 features <- read.table("./UCI HAR Dataset/features.txt")
-
+#importing lables of features
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
-
+#Data Mergin
 X_complete = rbind(X_train,X_test)
 y_complete = rbind(y_train,y_test)
 subject_complete = rbind(subject_train,subject_test)
 head(X_complete)
 
+#Extracting only the measurements on the mean and standard deviation for each measurement
+
 features_index<- grep("mean\\(\\)|std\\(\\)",features[,2],)
 X_complete<-X_complete[,features_index]
 
+
 colnames(X_complete)<-grep("mean\\(\\)|std\\(\\)",features[,2],value = T)
 
-head(X_complete)
+
 
 colnames(y_complete)<-"activity"
 y_complete$label<-factor(y_complete$activity, labels = as.character(activity_labels[,2]))
